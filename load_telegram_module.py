a0e942f878ba23274ac63eabb367198e1fe320e9
victorccaldas <victor_costa_caldas@hotmail.com>
2024-10-14 17:27:44 -0300

    Update load_telegram_module.py

--- load_telegram_module.py
+++ load_telegram_module.py
@@ -6,13 +6,16 @@ COPIAR ESTE ARQUIVO PARA A PASTA DO PROJETO E USÁ-LO PARA CARREGAR O MÓDULO DO
 -> from load_telegram_module import TelegramAPI, TelegramUtils
 '''
 
-# Force reinstall of the module from GitHub
+print("\nCarregando o módulo remoto do Telegram\n\n")
 
-# dynamically define the pip command according to the environment
+# dynamically define the pip command to be used according to the environment
 pip_command = [sys.executable, '-m', 'pip']
 
+# Force reinstall of the module from GitHub
 commands = pip_command + ['install', '--force-reinstall', '--no-cache-dir', 'git+https://github.com/victorccaldas/telegram-api.git']
 subprocess.run(commands)
 
 # Now import the remote module
 from telegram_api import TelegramAPI, TelegramUtils
+
+print('\n\n')
