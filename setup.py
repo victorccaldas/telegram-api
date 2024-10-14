3f197b37b97bda7659ec7510901ba10b5420ce53
victorccaldas <victor_costa_caldas@hotmail.com>
2024-10-14 15:26:14 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='telegram-api',              # Name of the package
+    version='0.1',                    # Version of your module
+    py_modules=['telegram_api'],      # Single file module
+    install_requires=[],              # Add dependencies here if any
+)
